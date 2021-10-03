4e007a3a1083739caf01904b1c14447a639c0c90
SabinKh <91114252+SabinKh@users.noreply.github.com>
2021-10-03 20:10:19 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region
+            //1.Verilmiş n və m (n<m) ədədləri arasında neçə tək ədəd olduğunu tapın.
+            //int n = 6;
+            //int m = 24;
+            //int count = 0;
+            //for (int i = n;i<=m;i++) 
+            //{
+            //    if (i % 2 == 1) 
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+            #endregion
+
+
+
+            #region
+            //1.Verilmiş n və m (n<m) ədədləri arasında neçə tək ədəd olduğunu tapın.
+            //int n = 6;
+            //int m = 0;
+            //if (n <= 0 || n == 1) 
+            //{
+
+            //    Console.WriteLine("ne sadedir ne murekkeb");
+
+            //}
+            //else if(n==2)
+            //{
+            //    Console.WriteLine("sade ededdir");
+
+            //}
+            //else
+            //{
+
+            //    for (int i = 2; i < n; i++)
+            //    {
+            //        if(n%i==0)
+            //        {
+            //            m = 1;
+            //            break;
+            //        }
+
+            //    }
+            //    if(m!=1)
+            //    {
+            //        Console.WriteLine("sade ededdir");
+
+            //    }
+            //    else
+            //    {
+
+            //        Console.WriteLine("murekkeb ededdir");
+
+            //    }
+
+            //}
+            #endregion
+
+
+
+
+            #region    
+            // Verilmiş n tam ədədinin neçə mərtəbəli olduğunu tapın.
+            //int n = 301;
+            //int c = 0;
+            //while (true)
+            //{
+            //    n = n / 10;//
+            //    c++;
+            //    if (n<=0)
+            //    {
+            //        Console.WriteLine(c);
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        continue;
+            //    }
+
+            //}
+            #endregion
+
+
+            #region
+            // Verilmiş n ədədinin 3 və 7 - ə bölünüb - bölünməməsini tapın.
+
+            //int n = 84;
+            //if (n%21==0)
+            //{
+            //    Console.WriteLine("bolunur");
+
+            //}
+            //else { 
+
+            //    Console.WriteLine("bolunmur");
+            //}
+
+            #endregion
+
+
+            #region
+            //// Verilmiş müsbət tam n ədədinin 2 - nin qüvvəti olub-olmamasını tapın.
+
+            //int n = 9;
+            //while (n % 2 == 0)
+            //{
+            //    n /= 2;
+            //}
+            //if (n == 1)
+            //{
+            //    Console.WriteLine("quvvetidir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("quvveti deyil");
+            //}
+            #endregion
+        }
+
+    }
+}
